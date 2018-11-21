029f1497bcd81b29ff175b26b9cee60266f6660c
igorkrutilin <irogkrutilin03@gmail.com>
2018-11-21 20:37:33 +0200

    comments upgraded

--- src/DrawingComponent.java
+++ src/DrawingComponent.java
@@ -1,4 +1,3 @@
-import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -6,7 +5,8 @@ import java.awt.Graphics2D;
 import javax.swing.JComponent;
 
 public class DrawingComponent extends JComponent {
-	public void paintComponent(Graphics g) {		
+	public void paintComponent(Graphics g) {
+		// <initializing>
 		Graphics2D g2d = (Graphics2D) g;
 		
 		Generator generator = new Generator();
@@ -15,18 +15,23 @@ public class DrawingComponent extends JComponent {
 		CaptchaGenerator captcha_generator = new CaptchaGenerator();
 		int frame_width = captcha_generator.get_frame_width();
 		int frame_height = captcha_generator.get_frame_height();
+		// </inititalizing>
 		
 		/*
-		  string width = 40 + width of last char
-		  so we can draw 4 chars with random width and 1 with constant width
-		  for this we need to have the same font for every last letter in every captcha we generate
-		*/
+		 * we need to know the width of our string to display it in the center of our frame
+		 * 
+		 * position of new letter = position of previous letter + 10 (position of the first letter is constant)
+		 * we have 5 letters so, string width = 40 + width of last char
+		 * so we can draw 4 chars with random width and 1 with constant width
+		 * for this we need to have the same font for every last letter in every captcha we generate
+		 */
 		
 		// getting string width to calculate captchas position on X axis
 		g2d.setFont(new Font("monospaced", Font.ITALIC, 25));
 		char last_char = captcha.charAt(4);
 		int last_char_width = g2d.getFontMetrics().stringWidth(String.valueOf(last_char));
 		int string_width = 40 + last_char_width;
+		
 		// displaying string
 		int pos_y = frame_height / 2 - 100;
 		for (int i = 0; i < captcha.length(); i++) {
