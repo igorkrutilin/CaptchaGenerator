b0e040b544a5fb2a66e0260e09c4d9de57abfdf7
igorkrutilin <irogkrutilin03@gmail.com>
2018-11-25 13:39:36 +0200

    input for user added

--- src/CaptchaGenerator.java
+++ src/CaptchaGenerator.java
@@ -3,37 +3,65 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 class CaptchaGenerator {
 	public final int WIDTH = 400;
 	public final int HEIGHT = 400;
+	
+	public String captcha;
 	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Captcha Generator");
 		CaptchaGenerator cg = new CaptchaGenerator();
 		frame.setSize(cg.WIDTH, cg.HEIGHT);
 		
-		JButton btn_new_captcha = new JButton("New captcha");
-		btn_new_captcha.setPreferredSize(new Dimension(cg.WIDTH, 50));
-		btn_new_captcha.addActionListener(new ActionListener() {
+		Generator generator = new Generator();
+		
+		JTextField input_captcha = new JTextField();
+		input_captcha.setMaximumSize(new Dimension(400, 100));
+		
+		JButton btn_submit = new JButton("Submit");
+		btn_submit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// after each click we create new DrawingComponent; otherwise, we will have the same captcha
-				DrawingComponent dc = new DrawingComponent();
-				frame.add(dc);
-				frame.setVisible(true);
+				if (input_captcha.getText().equals(cg.captcha)) {
+					JOptionPane.showMessageDialog(null, "Correct.");
+				} else {
+					JOptionPane.showMessageDialog(null, "Incorrect.");
+				}
 			}
 		});
 		
-		JPanel content = new JPanel(new BorderLayout());
-		content.add(btn_new_captcha, BorderLayout.PAGE_END);
+		JButton btn_new_captcha = new JButton("New captcha");
+		btn_new_captcha.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cg.captcha = generator.generate_word();
+				// after each click we create new DrawingComponent; otherwise, we will have the same captcha
+				DrawingComponent dc = new DrawingComponent(cg.captcha);
+				frame.add(dc);
+				frame.setVisible(true);
+				input_captcha.setText("");
+			}
+		});
+		
+		JPanel content = new JPanel();
+		content.setLayout(new BorderLayout());
+		Box horizontal_box = Box.createHorizontalBox();
+		horizontal_box.add(input_captcha);
+		horizontal_box.add(btn_submit);
+		horizontal_box.add(btn_new_captcha);
+		content.add(horizontal_box, BorderLayout.SOUTH);
 		
 		frame.setContentPane(content);
 		frame.setVisible(true);
-
-		DrawingComponent dc = new DrawingComponent();
+		
+		cg.captcha = generator.generate_word();
+		DrawingComponent dc = new DrawingComponent(cg.captcha);
 		frame.add(dc);
 	}
 }
--- src/DrawingComponent.java
+++ src/DrawingComponent.java
@@ -2,56 +2,72 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JComponent;
 
 public class DrawingComponent extends JComponent {
+	public String captcha;
+	
+	private List<String> captchas = new ArrayList<String>();
+	
+	DrawingComponent(String captcha) {
+		this.captcha = captcha;
+	}
+	
 	public void paintComponent(Graphics g) {
 		// <initializing>
 		Graphics2D g2d = (Graphics2D) g;
 		
 		Generator generator = new Generator();
-		String captcha = generator.generate_word();
 		
-		CaptchaGenerator captcha_generator = new CaptchaGenerator();
+		CaptchaGenerator captcha_generator = new CaptchaGenerator();		
 		int frame_width = captcha_generator.WIDTH;
 		int frame_height = captcha_generator.HEIGHT;
-		// </inititalizing>
+		// </initializing>
 		
-		/*
-		 * we need to know the width of our string to display it in the center of our frame
-		 * 
-		 * position of new letter = position of previous letter + 10 (position of the first letter is constant)
-		 * we have 5 letters so, string width = 40 + width of last char
-		 * so we can draw 4 chars with random width and 1 with constant width
-		 * for this we need to have the same font for every last letter in every captcha we generate
-		 */
-		
-		// getting string width to calculate captchas position on X axis
-		g2d.setFont(new Font("monospaced", Font.ITALIC, 25));
-		char last_char = captcha.charAt(4);
-		int last_char_width = g2d.getFontMetrics().stringWidth(String.valueOf(last_char));
-		int string_width = 40 + last_char_width;
-		
-		/*
-		 * when we want to regenerate captcha for x times, there will be x captchas displayed at once
-		 * so, before drawing new one, we fill our screen with white color
-		 * that will hide previous captchas
-		 */
-		g2d.setColor(new Color(255, 255, 255));
-		g2d.fillRect(0, 0, frame_width, frame_height);
-		
-		// displaying string
-		int pos_y = frame_height / 2 - 100;
-		for (int i = 0; i < captcha.length(); i++) {
-			g2d.setColor(generator.generate_color());
-			if (i != 4) {
-				g2d.setFont(generator.generate_font());
-			} else {
-				g2d.setFont(new Font("monospaced", Font.ITALIC, 25));
+		// without this if statement we will have the same captcha in different fonts and colors all the time
+		if (!this.captchas.contains(this.captcha)) {
+			this.captchas.add(this.captcha);
+			
+			/*
+			 * we need to know the width of our string to display it in the center of our frame
+			 * 
+			 * position of new letter = position of previous letter + 10 (position of the first letter is constant)
+			 * we have 5 letters so, string width = 40 + width of last char
+			 * so we can draw 4 chars with random width and 1 with constant width
+			 * for this we need to have the same font for every last letter in every captcha we generate
+			 */
+			
+			// getting string width to calculate captchas position on X axis
+			g2d.setFont(new Font("monospaced", Font.ITALIC, 25));
+			char last_char = this.captcha.charAt(4);
+			int last_char_width = g2d.getFontMetrics().stringWidth(String.valueOf(last_char));
+			int string_width = 40 + last_char_width;
+			
+			/*
+			 * when we want to regenerate captcha for x times, there will be x captchas displayed at once
+			 * so, before drawing new one, we fill our screen with white color
+			 * that will hide previous captchas
+			 */
+			
+			// filling screen
+			g2d.setColor(new Color(255, 255, 255));
+			g2d.fillRect(0, 0, frame_width, frame_height);
+			
+			// displaying string
+			int pos_y = frame_height / 2 - 100;
+			for (int i = 0; i < this.captcha.length(); i++) {
+				g2d.setColor(generator.generate_color());
+				if (i != 4) {
+					g2d.setFont(generator.generate_font());
+				} else {
+					g2d.setFont(new Font("monospaced", Font.ITALIC, 25));
+				}
+				int pos_x = frame_width / 2 - string_width / 2 + i * 10;
+				g2d.drawString(String.valueOf(this.captcha.charAt(i)), pos_x, pos_y);
 			}
-			int pos_x = frame_width / 2 - string_width / 2 + i * 10;
-			g2d.drawString(String.valueOf(captcha.charAt(i)), pos_x, pos_y);
 		}
 	}
 }
